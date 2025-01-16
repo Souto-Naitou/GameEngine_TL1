672460c7233a36495f37a192e8c4ff78d4d625d3
Souto-Naitou <k023g0031@g.neec.ac.jp>
2025-01-16 14:09:39 +0900

    WinToolsのアプデ

--- Engine/Externals/WinTools/WinTools.h
+++ Engine/Externals/WinTools/WinTools.h
@@ -5,7 +5,21 @@
 class WinTools
 {
 public:
+    WinTools(WinTools&) = delete;
+    WinTools& operator=(const WinTools&) = delete;
+    WinTools(WinTools&&) = delete;
+    WinTools& operator=(WinTools&&) = delete;
+
+    static WinTools* GetInstance()
+    {
+        static WinTools instance;
+        return &instance;
+    }
+
+    std::string OpenFileDialog();
+
+private:
     WinTools();
     ~WinTools();
-    std::string OpenFileDialog();
+
 };
