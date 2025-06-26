fb43744961ee0e3e8c4ac03cb6a0c3cecb1211de
Souto-Naitou <k023g0031@g.neec.ac.jp>
2025-06-26 13:14:40 +0900

    fix: Vignette修正

--- Engine/Utility/Debug/dbgutl.cpp
+++ Engine/Utility/Debug/dbgutl.cpp
@@ -3,14 +3,14 @@
 
 std::string utl::debug::generate_name_default(void* _ptr)
 {
-    std::string result = "unnamed#";
+    std::string result = "unnamed##";
     result += utl::string::to_string(_ptr);
     return result;
 }
 
 std::string utl::debug::generate_name(const std::string& _name, void* _ptr)
 {
-    std::string result = _name + '#';
+    std::string result = _name + "##";
     result += utl::string::to_string(_ptr);
     return result;
 }
